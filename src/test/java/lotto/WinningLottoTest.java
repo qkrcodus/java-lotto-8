a2d0dd6559ce0cd6b4ad3f25eefe4e73c9dd4cb7
qkrcodus <qkrcodus0722@gmail.com>
2025-11-03 02:33:51 +0900

    test(WinningLotto): WinningLotto 테스트 코드 작성
    - 사용자가 입력으로 당첨 로또 생성
    - 보너스 번호가 1~45 사이 값이 아니거나 로또 번호와 중복되면 예외 발생
    - 로또 등수 확인

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,86 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+    @Test
+    void 당첨_로또를_생성한다() {
+        WinningLotto winningNumbers = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        assertThat(winningNumbers).isNotNull();
+    }
+
+    @Test
+    void 보너스_번호가_1보다_작으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_45보다_크면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_1등() {
+        WinningLotto winningNumbers = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_2등() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningNumbers = new WinningLotto(winningLotto, 7);
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_3등() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningNumbers = new WinningLotto(winningLotto, 7);
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_4등() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningNumbers = new WinningLotto(winningLotto, 7);
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_5등() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningNumbers = new WinningLotto(winningLotto, 7);
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    void 구매한_로또의_당첨_등수를_판정한다_낙첨() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningNumbers = new WinningLotto(winningLotto, 7);
+        Lotto purchasedLotto = new Lotto(List.of(8, 9, 10, 11, 12, 13));
+        Rank rank = winningNumbers.match(purchasedLotto);
+        assertThat(rank).isEqualTo(Rank.NONE);
+    }
+}
