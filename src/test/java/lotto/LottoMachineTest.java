c0cf20cb7c3ad62d31ec4a2643baf3d59d4fe066
qkrcodus <qkrcodus0722@gmail.com>
2025-11-03 03:21:40 +0900

    test(LottoMachine): LottoMachine 테스트 코드 구현
    - 원하는 개수만큼 발행되는지 확인
    - 발행된 로또의 숫자는 6개고, 1~45 사이며 중복이 없는지 확인

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoMachineTest {
+
+    @Test
+    void 로또를_1개_발행한다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        int count = 1;
+        List<Lotto> lottos = lottoMachine.generate(count);
+        assertThat(lottos).hasSize(1);
+    }
+
+    @Test
+    void 로또를_여러개_발행한다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        int count = 5;
+        List<Lotto> lottos = lottoMachine.generate(count);
+        assertThat(lottos).hasSize(5);
+    }
+
+    @Test
+    void 발행된_로또는_6개의_번호를_가진다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = lottoMachine.generate(1);
+        Lotto lotto = lottos.get(0);
+        assertThat(lotto.getNumbers()).hasSize(6);
+    }
+
+    @Test
+    void 발행된_로또_번호는_1부터_45_사이다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = lottoMachine.generate(1);
+        Lotto lotto = lottos.get(0);
+        assertThat(lotto.getNumbers())
+                .allMatch(number -> number >= 1 && number <= 45);
+    }
+
+    @Test
+    void 발행된_로또_번호는_중복되지_않는다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = lottoMachine.generate(1);
+        Lotto lotto = lottos.get(0);
+        List<Integer> numbers = lotto.getNumbers();
+        assertThat(numbers).doesNotHaveDuplicates();
+    }
+}
