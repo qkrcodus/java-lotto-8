93e4254fa4f0d78225e780c868a73b2dd6118c52
qkrcodus <qkrcodus0722@gmail.com>
2025-11-03 02:24:18 +0900

    feat(Rank): enum Rank 기능 구현
    - enum의 내장 메서드인 values()와 stream을 사용하여 등수 판정 로직 구현

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean requireBonus;
+    private final int prizeMoney;
+
+    Rank(int matchCount, boolean requireBonus, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.requireBonus = requireBonus;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Rank valueOf(int matchCount, boolean bonusMatch) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matches(matchCount, bonusMatch))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean matches(int matchCount, boolean bonusMatch) {
+        if (this == NONE) {
+            return matchCount < FIFTH.matchCount;
+        }
+        if (matchCount == FIRST.matchCount) {
+            return this == FIRST;
+        }
+        if (matchCount == SECOND.matchCount) {
+            return this.matchCount == 5 && this.requireBonus == bonusMatch;
+        }
+
+        return matchCount == this.matchCount && !this.requireBonus;
+    }
+
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}
